1366f6f2ae77603cf1cb17505955e22008d23da6
taru khan <tarukhan19@gmail.com>
2019-12-17 19:08:26 +0530

    Initial commit

---
+++ app/src/main/java/com/effseele/edukosh/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.effseele.edukosh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.effseele.edukosh.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+ActivityLoginBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        if (android.os.Build.VERSION.SDK_INT >= 21)
+        {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
+        }
+    }
+}
---
+++ app/src/main/java/com/effseele/edukosh/MainActivity.java
@@ -0,0 +1,46 @@
+package com.effseele.edukosh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT =8000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT >= 21)
+        {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
+        }
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+
+
+                    Intent intent=new Intent(MainActivity.this,
+                            LoginActivity.class);
+                    intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.trans_left_in,
+                            R.anim.trans_left_out);
+                    finish();
+
+
+
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
